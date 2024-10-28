bb7f00636d019fe3292acc6738e2b4e482c6cf00
Martyna Mazur <smileymarti3@gmail.com>
2024-10-28 22:04:59 +0100

    sekcja 4 skonczona

--- 04-Functions/4-8.py
+++ 04-Functions/4-8.py
@@ -1,25 +1,32 @@
 
 def time_string(hours, minutes, time_format):
     if time_format == '12':
-        if hours <= 12:
+        if hours == 0:
+            hours=12
+            midnight='AM'
+        elif hours < 12:
             midnight = 'AM'
+        elif hours == 12:
+            midnight = 'PM'
         else:
             midnight = 'PM'
-            hours = hours/2
+            hours = hours-12
     
         if minutes < 10:
             result = (f'{hours}:0{minutes}{midnight}')
-        result = (f'{hours}:{minutes}{midnight}')
-        
+        else:
+            result = (f'{hours}:{minutes}{midnight}') 
     else:
         if hours < 10:
             if minutes < 10:
                 result = (f'0{hours}:0{minutes}')
-            result = (f'0{hours}:{minutes}')
+            else:
+                result = (f'0{hours}:{minutes}')
         else:
             if minutes < 10:
                 result = (f'{hours}:0{minutes}')
-            result = (f'{hours}:{minutes}')
+            else:
+                result = (f'{hours}:{minutes}')
 
     return result
 
@@ -27,7 +34,7 @@ hours = int(input('Enter hours: '))
 minutes = int(input('Enter mintues: '))
 time_format = input('Enter time format (12 or 24): ')
 
-time = time_string(hours,minutes, time_format)
+time = time_string(hours, minutes, time_format)
 
 print('The time is: ', time)
 
