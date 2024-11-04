bee0c5bcab88e093c919f98918002755251ada68
Martyna Mazur <smileymarti3@gmail.com>
2024-11-04 19:36:30 +0100

    zmiany

--- 04-Functions/5-3.py
+++ 04-Functions/5-3.py
@@ -1,11 +1,11 @@
-import keyboard 
+import keyboard
 
 
 first_name = keyboard.input_string('Enter name: ')
-last_name = keyboard.inpu
-age = ...
+last_name = keyboard.input_string('Enter last name:')
+age = keyboard.input_integer('Enter your age')
 salary = ...
-is_salary_hidden = input_boolean('Hide salary? (y/n)')
+is_salary_hidden = keyboard.input_boolean('Hide salary? (y/n)')
 
 # Prints employee's record
 print('DATA RECORD')
