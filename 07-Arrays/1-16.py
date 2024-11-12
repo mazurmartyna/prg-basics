f499bf3972f0e90196803ec589ffe8ae187d70c8
Martyna Mazur <smileymarti3@gmail.com>
2024-11-12 14:22:12 +0100

    sekcja 1 zadania skonczone

--- 07-Arrays/1-16.py
+++ 07-Arrays/1-16.py
@@ -1,8 +1,13 @@
 # Sort the data from lowest to highest value
 distances_traveled = [120, 150, 180, 90, 200, 175, 160]
-sorted(distances_traveled)
+print(sorted(distances_traveled))
+
 # Sort the data in descending order, from highest to lowest value
 daily_temperatures = [16, 17, 15, 14, 18, 19, 17, 15, 16, 18]
+print(sorted(daily_temperatures, reverse=True))
+
 # Sort the data in ascending order
 file_names = ["report.docx", "presentation.pptx", "data.csv", "photo.jpg", "notes.txt",
    "invoice.pdf", "resume.docx", "budget.xlsx", "meeting.mp4", "schedule.pdf"]
+
+print(sorted(file_names))
